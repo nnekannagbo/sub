3181f309ccd5ceaea9a7a937f7995d54cecaca9e
Nneka Nnagbo <nnekakassandra@gmail.com>
2022-10-23 17:06:25 -0400

    Update random-thoughts.js

--- js/random-thoughts.js
+++ js/random-thoughts.js
@@ -5,7 +5,8 @@ var titles = [
     'I was thinking about freedom and form in my dream last night. Form was my body and freedom was my soul.',
     'just cause you perceive it as so does not make it so.',
     'Co-star told me to express my inner world more.',
-    'I make many lists'
+    'I make many lists',
+    'are footnotes accessible to screen readers? what about an asterisk*?'
     //'the word wood at one point meant violently insane'
 ];
 
